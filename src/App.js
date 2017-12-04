057716cf72ab4d9fabd614301115a2bc367ac548
Andrew Bodey <a.s.bodey@gmail.com>
2017-12-04 15:35:48 -0800

    added plugin for react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+state = {
+    name: 'World'
+}
+
+handleChange = ({ target }) => this.setState({ [target.name]: target.value})
+
+    render() {
+        return(
+            <main>
+            <div>Hello {this.state.name}</div>
+            <div>
+            <input name="name" value={this.state.name} onChange={this.handleChange}/>
+            </div>
+            </main>    
+        )
+    }
+}
