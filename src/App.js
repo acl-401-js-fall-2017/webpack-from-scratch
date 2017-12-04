06eaed7b60edeea7c3f87e745d8739b741090977
Andrew Bodey <a.s.bodey@gmail.com>
2017-12-04 15:45:57 -0800

    added salutation and css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import './App.css';
 
 export default class App extends Component {
 state = {
+    salutation: 'Hello',
     name: 'World'
 }
 
 handleChange = ({ target }) => this.setState({ [target.name]: target.value})
-
-    render() {
+render() {
+    const { salutation, name } = this.state
         return(
             <main>
-            <div>Hello {this.state.name}</div>
+            <div className="greeting" >{salutation} {name}</div>
             <div>
-            <input name="name" value={this.state.name} onChange={this.handleChange}/>
+            <input name="salutation" value={salutation} onChange={this.handleChange}/>
+            <input name="name" value={name} onChange={this.handleChange} />
             </div>
             </main>    
         )
