4a41acecb2f4b59df724c9967eeaa923cb697f76
mugworks <greenwood.pdx@gmail.com>
2017-12-04 14:39:59 -0800

    bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
-
+const HTMLPlugin = require('html-webpack-plugin');
 module.exports = {
+  devtool: 'source-map',
   entry: `${__dirname}/src/main.js`,
   output: {
     path: `${__dirname}/build`,
-    filename: 'bundle.js'
+    filename: 'bundle-[hash].js'
   },
   devServer: {
     contentBase: `${__dirname}/build`
-  }
+  },
+  plugins: [
+    new HTMLPlugin()
+  ]
 }
