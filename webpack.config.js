94886bae6312d9ff1366b5b53ab5621bda01d2e3
David Nishikawa <triptype@gmail.com>
2017-12-04 14:28:17 -0800

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ module.exports = {
     entry: `${__dirname}/src/main.js`,
     output: {
         path: `${__dirname}/build`,
-        filename: 'bundle.js',
+        filename: 'bundle.js'
     },
+    devServer: {
+        contentBase: `${__dirname}/build`
+    }
 }
