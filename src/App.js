523fadf111ca119d973582e7076f79f13594ac3d
Robin Miller <rmillerpdx@gmail.com>
2017-12-04 15:43:00 -0800

    fix input form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 export default class App extends Component {
   state = {
@@ -10,8 +11,8 @@ export default class App extends Component {
   render () {
     return (
       <main>
-      <div>hhhiiii {this.state.name}</div>
-      <input name="name" value={name} onChange={this.handleChange}/>
+      <div className="greeting">hh8hiiii {this.state.name}</div>
+      <input name="name" value={this.state.name} onChange={this.handleChange}/>
       </main>
     )
   }
