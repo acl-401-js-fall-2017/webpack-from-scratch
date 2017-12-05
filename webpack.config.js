908b20ad09e6241f6d58a9a0807057e89cef667f
mirrorapps <eli.speigel@gmail.com>
2017-12-04 16:02:37 -0800

    extract to own css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   devtool: 'source-map',
@@ -11,6 +12,7 @@ module.exports = {
     contentBase: `${__dirname}/build`
   },
   plugins: [
+    new ExtractTextPlugin('bundle.css'),
     new HTMLPlugin({ template: `${__dirname}/src/index.html`})
   ],
   module: {
@@ -22,7 +24,7 @@ module.exports = {
       },
       {
         test:/\.css$/,
-        loader: ['style-loader', 'css-loader'],
+        loader: ExtractTextPlugin.extract(['css-loader']),
       }
     ]
   }
