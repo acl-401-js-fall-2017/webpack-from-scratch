46c4e73e7a72913603f86b1e3bf69cc560d4ff6a
mugworks <greenwood.pdx@gmail.com>
2017-12-04 16:22:50 -0800

    add scss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import Greeting from './Greeting';
+import './App.scss';
 
 export default class App extends Component {
-  // 
+
   state = {
+    salutation: 'Hello',
     name: 'World'
   }
+
   handleChange = ({ target }) => this.setState({ [target.name]: target.value });
 
   render() {
+    const { salutation, name } = this.state;
+
     return (
       <main>
-      <div>Hello {this.state.name}</div>
-      <div>
-        <input name="name" value={this.state.name} onChange={this.handleChange}/>
-      </div>
+        <h1>Greeting App</h1>
+        <Greeting salutation={salutation} name={name}/>
+        <div>
+          <input name="salutation" value={salutation} onChange={this.handleChange}/>
+          <input name="name" value={name} onChange={this.handleChange}/>
+        </div>
       </main>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   devtool: 'source-map',
   entry: `${__dirname}/src/main.js`,
@@ -10,6 +12,7 @@ module.exports = {
     contentBase: `${__dirname}/build`
   },
   plugins: [
+    new ExtractTextPlugin('bundle.css'),
     new HTMLPlugin({ template: `${__dirname}/src/index.html` })
   ],
   module: {
@@ -18,7 +21,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
       }
     ]
   }
-}
+};
