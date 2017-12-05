a97cda0f10b6deb2faa7fdd7169890e62c8dd35b
Robin Miller <rmillerpdx@gmail.com>
2017-12-04 16:50:40 -0800

    fix scss in greetingForm

--- src/GreetingForm.js
+++ src/GreetingForm.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import './GreetingForm.scss'
+import style from './GreetingForm.scss';
+
 export default class GreetingForm extends Component {
   render() {
     const { salutation, name, onChange } = this.props;
     
     return (
-      <div>
-        <div><label className="greeting">Salutation:</label> <input name="salutation" value={salutation} onChange={onChange}/></div>
-        <div>Input <input name="name" value={name} onChange={onChange}/></div>
+      <div className={style.greeting}>
+        <div>Salutation: 
+          <input name="salutation" value={salutation} onChange={onChange}/>
+        </div>
+        <div>
+          Input: <input name="name" value={name} onChange={onChange}/>
+        </div>
       </div>
     );
   }
