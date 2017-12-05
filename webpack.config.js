cc06929708f69227ed9c3faefa380d4634b03e70
ckrountree <jmail0207@gmail.com>
2017-12-04 16:30:27 -0800

    update css and scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -11,13 +12,24 @@ module.exports = {
     contentBase: `${__dirname}/build`
   },
   plugins: [
+    new ExtractTextPlugin('bundle.css'),
     new HTMLPlugin({ template: `${__dirname}/src/index.html`})
   ],
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {  
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(['css-loader']),
+      },
+      {  
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+      }
+    ]
   }
 }
